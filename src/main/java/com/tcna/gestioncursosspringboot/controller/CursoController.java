46332fb1ecc8937ef634ebd5237fdbc40da671e3
Bayron Ramirez <norbydro@gmail.com>
2023-11-30 21:46:44 -0500

    Paginación y búsqueda

--- src/main/java/com/tcna/gestioncursosspringboot/controller/CursoController.java
+++ src/main/java/com/tcna/gestioncursosspringboot/controller/CursoController.java
@@ -6,15 +6,22 @@ import com.tcna.gestioncursosspringboot.reports.CursoExporterPDF;
 import com.tcna.gestioncursosspringboot.repository.CursoRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -28,11 +35,38 @@ public class CursoController {
         return "redirect:/cursos";
     }
 
-    @GetMapping("/cursos")
+    /*@GetMapping("/cursos")        //Normal
     public String listarCursos(Model model){
         List<Curso> cursos = cursoRepository.findAll();
         cursos = cursoRepository.findAll();
         model.addAttribute("cursos", cursos);
+        return "cursos";
+    }*/
+    @GetMapping("/cursos")        //Con paginación
+    public String listarCursos(Model model, @Param("keyword") String keyword, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "3") int size){
+        try{
+            List<Curso> cursos = new ArrayList<>();
+            Pageable paging = PageRequest.of(page-1, size);     //Sacar de Domain
+
+            Page<Curso> pageCursos = null;
+
+            if(keyword == null){
+                pageCursos = cursoRepository.findAll(paging);
+            }else{
+                pageCursos = cursoRepository.findByTituloContainingIgnoreCase(keyword, paging);
+                model.addAttribute("keyword", keyword);
+            }
+            cursos = pageCursos.getContent();
+
+            model.addAttribute("cursos", cursos);
+
+            model.addAttribute("currentPage", pageCursos.getNumber()+1);
+            model.addAttribute("totalItems", pageCursos.getTotalElements());
+            model.addAttribute("totalPages", pageCursos.getTotalPages());
+            model.addAttribute("pageSize", size);
+        }catch (Exception exception){
+            model.addAttribute("message", exception.getMessage());
+        }
         return "cursos";
     }
 
@@ -51,6 +85,32 @@ public class CursoController {
             redirectAttributes.addFlashAttribute("message", "El curso ha sido guardado con éxito");
         }catch (Exception e){
             redirectAttributes.addAttribute("message", e.getMessage());
+        }
+        return "redirect:/cursos";
+    }
+
+    @GetMapping("/cursos/{id}")
+    public String editarCurso(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes){
+        try{
+            Curso curso = cursoRepository.findById(id).get();
+
+            model.addAttribute("curso",curso);
+            model.addAttribute("pageTitle","Editar curso : " + id);
+
+            return "curso_form";
+        }catch (Exception e){
+            redirectAttributes.addFlashAttribute("message",e.getMessage());
+            return "redirect:/cursos";
+        }
+    }
+
+    @GetMapping("/cursos/delete/{id}")
+    public String eliminarCurso(@PathVariable Integer id,Model model,RedirectAttributes redirectAttributes){
+        try{
+            cursoRepository.deleteById(id);
+            redirectAttributes.addFlashAttribute("message","El curso ha sido eliminado");
+        }catch (Exception exception){
+            redirectAttributes.addFlashAttribute("message",exception.getMessage());
         }
         return "redirect:/cursos";
     }
